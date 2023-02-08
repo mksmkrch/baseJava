8c31be44f8b1c100b7f00743e6eb6cc9664e8323
mksmkrch <wedetttt@gmail.com>
2023-02-08 17:38:07 +0300

    Домашка (тестовое)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,48 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    int size = 0;
     void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        int index = Math.max(size, 0);
+        storage[index] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(uuid, storage[i].toString())) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(uuid, storage[i].toString())) {
+                storage[i] = storage[size-1];
+                storage[size-1] = null;
+                size --;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
